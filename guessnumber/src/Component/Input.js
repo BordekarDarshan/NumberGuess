fbb42c5ebb45cb7f4d6975da2ef7c6164c247954
darshan bordekar <darshanbordekar1819@gmail.com>
2020-04-01 12:44:26 +0530

    Phase 1 Complete

--- guessnumber/src/Component/Input.js
+++ guessnumber/src/Component/Input.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-
 export class Input extends Component {
   constructor(props) {
     super(props);
@@ -12,29 +11,27 @@ export class Input extends Component {
   getInput = e => {
     this.setState({ [e.target.name]: e.target.value });
   };
+
   checkVal = () => {
-    let convert = Number(this.state.inputVal);
-
-    let diff = Math.round(Math.random() * this.state.limit) - convert;
-
+    let UserInput = Number(this.state.inputVal);
+    let Difference = Math.round(Math.random() * this.state.limit) - UserInput;
     let Balance = this.setState({ ...this.state.limit });
 
-    if (diff === 0) {
+    if (Difference === 0) {
       alert("Correct");
-
       this.setState({ limit: this.state.limit + 100 });
-    } else if (diff >= 1 && diff <= 4) {
+    } else if (Difference >= 1 && Difference <= 4) {
       alert("Hot");
       return Balance;
-    } else if (diff >= 5 && diff <= 14) {
+    } else if (Difference >= 5 && Difference <= 14) {
       alert("Warm");
       return Balance;
-    } else if (diff >= 15) {
+    } else if (Difference >= 15) {
       alert("Cold");
       return Balance;
     }
-    console.log("Limit", this.state.limit);
   };
+
   render() {
     return (
       <div>
